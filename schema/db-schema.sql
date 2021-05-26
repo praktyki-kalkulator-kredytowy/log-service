3a743530610d82e7502428e908c30353d353ca56
markopolo139 <marekseget@onet.pl>
2021-05-26 09:00:42 +0200

    refractor(schema): change data base schema

--- schema/db-schema.sql
+++ schema/db-schema.sql
@@ -1,9 +1,6 @@
 create table schedule_calculation_event(
     schedule_calculation_event_id int not null primary key auto_increment,
-    schedule_calculation_event_date date not null
-);
-create table schedule_configurations(
-    schedule_calculation_event_id int not null,
+    schedule_calculation_event_date date not null,
     capital double not null,
     installment_type Enum('CONSTANT','DECREASING') not null,
     installment_amount int not null,
@@ -11,37 +8,23 @@ create table schedule_configurations(
     withdrawal_date date not null,
     commission_rate double not null,
     insurance boolean not null,
-    age int not null,
-    key (schedule_calculation_event_id),
-    constraint configuration_to_order foreign key (schedule_calculation_event_id) references schedule_calculation_event(schedule_calculation_event_id)
-);
-create table installments(
-    schedule_calculation_event_id int not null,
-    installment_index int not null,
-    installment_date date not null,
-    capital_installment double not null,
-    interest_installment double not null,
-    remaining_debt double not null,
-    key (schedule_calculation_event_id),
-    constraint installment_to_order foreign key (schedule_calculation_event_id) references schedule_calculation_event(schedule_calculation_event_id)
-);
-create table insurance_premiums(
-    schedule_calculation_event_id int not null,
-    insurance_premium_index int not null,
-    insurance_premium_date date not null,
-    insurance_premium_value double not null,
-    key (schedule_calculation_event_id),
-    constraint insurance_to_order foreign key (schedule_calculation_event_id) references schedule_calculation_event(schedule_calculation_event_id)
-);
-create table schedule_summary(
-    schedule_calculation_event_id int not null,
+    age int null,
     sum_up_capital_installment double not null,
     loan_paid_out_amount double not null,
     commission_amount double not null,
-    insurance_total_cost double not null,
+    insurance_total_cost double null,
     loan_total_cost double not null,
-    aprc double not null,
-    key (schedule_calculation_event_id),
-    constraint summary_to_order foreign key (schedule_calculation_event_id) references schedule_calculation_event(schedule_calculation_event_id)
+    aprc double not null
 );
-
+create table payments(
+    payment_id int not null primary key auto_increment,
+    payment_index int not null,
+    payment_date date not null,
+    capital_payment double not null,
+    interest_payment double not null,
+    remaining_debt double not null,
+    insurance_premium_value double null,
+    schedule_calculation_event_id int not null,
+    key (schedule_calculation_event_id),
+    constraint payment_to_order foreign key (schedule_calculation_event_id) references schedule_calculation_event(schedule_calculation_event_id)
+);
